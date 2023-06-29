0517b6bd0711907fd34acf54010aceae537ad11c
Purple-Code-sh <purple.code.sh@gmail.com>
2023-06-29 02:48:08 -0600

    adding all vitest configuration to viteConfig and tsConfig, the project its ready to start testing

---
+++ src/components/Accordion.test.tsx
@@ -0,0 +1,10 @@
+/* // To avoid including allways the next imports are using globals in vite and ts configs.
+import { describe, expect, test } from "vitest"; */
+
+import Accordion from "./Accordion";
+
+describe("Accordion", () => {
+  test("should add two numbers", () => {
+    expect(1 + 3).toBe(2);
+  });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+/// <reference types="Vite/client" />
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    environment: "jsdom",
+    globals: true,
+  },
+});
