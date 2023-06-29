d3a24c48b14d7f69e600e0de456d335cd0d8fa6c
Purple-Code-sh <purple.code.sh@gmail.com>
2023-06-29 03:01:42 -0600

    adding react-testing-library, configs to package.json and making a test to verify rendering of a specific component

--- src/components/Accordion.test.tsx
+++ src/components/Accordion.test.tsx
@@ -1,10 +1,17 @@
 /* // To avoid including allways the next imports are using globals in vite and ts configs.
 import { describe, expect, test } from "vitest"; */
 
+import { render, screen } from "@testing-library/react";
 import Accordion from "./Accordion";
 
 describe("Accordion", () => {
-  test("should add two numbers", () => {
-    expect(1 + 3).toBe(2);
+  test("should render the component", () => {
+    render(
+      <Accordion title="shm-A">
+        <h2>Subtitle</h2>
+        <span>Some other content</span>
+      </Accordion>
+    );
+    expect(screen.getByText("shm-A")).toBeDefined();
   });
 });
--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { render } from "@testing-library/react";
 
 type AccordionProps = {
   title: string;
