7a014bf3a565de6331105343a40790c271d4bf3e
Purple-Code-sh <purple.code.sh@gmail.com>
2023-06-29 20:53:50 -0600

    adding more cards and some styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,26 @@
+import Accordion from "./components/Accordion/Accordion";
+import "./App.css";
+
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <section className="welcomeSection">
+        <Accordion title="More Information">
+          <h3>Ofertas en tendencia</h3>
+          <p>Holaaa</p>
+        </Accordion>
+        <Accordion title="More Information">
+          <h3>Ofertas en salud</h3>
+          <p>Holaaa</p>
+        </Accordion>
+        <Accordion title="More Information">
+          <h3>Descuentos en tecnologia</h3>
+          <p>Holaaa</p>
+        </Accordion>
+        <Accordion title="Inicia sesión para vivir tu mejor experiencia">
+          <p></p>
+        </Accordion>
+      </section>
+    </div>
+  );
 }
--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { render } from "@testing-library/react";
+
+import "./accordionStyle.css";
 
 type AccordionProps = {
   title: string;
@@ -10,8 +11,8 @@ export default function Accordion({ title, children }: AccordionProps) {
   const [open, setOpen] = useState(false);
 
   return (
-    <div>
-      <div>
+    <div className="accordion">
+      <div className="cardHeader">
         <h3>{title}</h3>
 
         {/* If "open" state it's true show "Close" text, else show "Open" */}
@@ -19,13 +20,14 @@ export default function Accordion({ title, children }: AccordionProps) {
           onClick={() => {
             setOpen(!open);
           }}
+          className="actionButton"
         >
           {open ? "Close" : "Open"}
         </button>
       </div>
 
       {/* If "open" state it's true show div with children */}
-      {open && <div>{children}</div>}
+      {open && <div className="card ">{children}</div>}
     </div>
   );
 }
