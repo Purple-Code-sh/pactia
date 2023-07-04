fb84a11bc129cb1f2edd3d895d879c521ce0e336
Purple-Code-sh <purple.code.sh@gmail.com>
2023-07-04 16:26:36 -0600

    implementing that was maked in testing to app code

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,29 @@
+//components
 import Accordion from "./components/Accordion/Accordion";
+
+//data
+import cardsInfo from "./data/cardsInfo.json";
+
+//style
 import "./App.css";
 
 export default function App() {
+  const showCards = cardsInfo.map((element) => {
+    return (
+      <Accordion title="Hide info">
+        <h3 role="sectionHeader">{element.header}</h3>
+        <div className="toGrow">
+          <div className="fromGrow">box with flexible height</div>
+        </div>
+        <div role="articleDescription">
+          <p>First description paragraph</p>
+        </div>
+      </Accordion>
+    );
+  });
   return (
     <div>
-      <section className="welcomeSection">
-        <Accordion title="More Information">
-          <h3 className="sectionHeader">Ofertas en tendencia</h3>
-          <div className="toGrow">
-            <div className="fromGrow">box with flexible height</div>
-          </div>
-          <div className="articleDescription">
-            <p>Audífonos Gamer y Accesorios para Consolas</p>
-          </div>
-        </Accordion>
-
-        <Accordion title="More Information">
-          <h3>Ofertas en salud</h3>
-          <div className="articleDescription">
-            <p>Oferta en serums, cremas faciales y fibras capilares</p>
-          </div>
-        </Accordion>
-
-        <Accordion title="More Information">
-          <h3>Descuentos en tecnologia</h3>
-          <div className="articleDescription">
-            <p>Electrónica, Cámaras, Fotografía y más</p>
-          </div>
-        </Accordion>
-
-        <Accordion title="Inicia sesión para vivir tu mejor experiencia">
-          <div className="articleDescription">
-            <p>Promoción pagada</p>
-          </div>
-        </Accordion>
-      </section>
+      <section className="welcomeSection">{showCards}</section>
     </div>
   );
 }
