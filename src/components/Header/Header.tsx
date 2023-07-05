31a963d7720bf4627c7b02de8cc4fc812d3d9b6c
Purple-Code-sh <purple.code.sh@gmail.com>
2023-07-05 14:12:39 -0600

    header with flexible width

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,4 @@
 import "./headerStyle.css";
-//utilities
-import useWindowDimension from "../../utilities/useWindowDimension";
-
 export default function Header() {
-  const { width } = useWindowDimension();
-  const windowWidth = width.toString() + "px";
-  return (
-    <div className="main" style={{ maxWidth: windowWidth, width: windowWidth }}>
-      Header
-    </div>
-  );
+  return <div className="main">Header</div>;
 }
