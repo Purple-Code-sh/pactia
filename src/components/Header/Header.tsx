737dbd19ce6d1dee7e17a5a1a8d535a26677cc89
Purple-Code-sh <purple.code.sh@gmail.com>
2023-07-06 00:34:52 -0600

    header styles passed to tailwind

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,7 @@
-import "./headerStyle.css";
 export default function Header() {
   return (
-    <div className="main">
-      <div className="some clase2 md:clase1 ">Header</div>
+    <div className="h-16 w-full flex flex-nowrap items-center bg-ssTheme-blackBlue p-2 ">
+      <div className=" bg-ssTheme-whiteBlue">Header</div>
     </div>
   );
 }
