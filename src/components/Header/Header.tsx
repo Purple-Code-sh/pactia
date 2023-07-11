bec909071e8eb9bfe699b02c47419938760a120c
Purple-Code-sh <purple.code.sh@gmail.com>
2023-07-10 23:29:47 -0600

    logo into header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,11 @@
+import logoPactia from "../../assets/logoPactia.svg";
+
 export default function Header() {
   return (
-    <div className="h-16 w-full flex flex-nowrap items-center bg-ssTheme-blackBlue p-2 text-ssTheme-whiteBlue">
-      <div className=" bg-ssTheme-blue">Header</div>
+    <div className="h-16 w-full flex flex-nowrap items-center bg-ssTheme-blackBlue py-2 px-5  text-ssTheme-whiteBlue">
+      <div className=" bg-ssTheme-blue h-full rounded-full">
+        <img src={logoPactia} alt="logo Pactia.com" className="h-full" />
+      </div>
     </div>
   );
 }
