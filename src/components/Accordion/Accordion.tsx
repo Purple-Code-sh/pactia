ab7fbd5a34a06c6f2b630a44cba6a008a4628be3
Purple-Code-sh <purple.code.sh@gmail.com>
2023-07-10 23:36:34 -0600

    color borders and fixing logo styles

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -6,7 +6,7 @@ export default function Accordion({
   action,
 }: AccordionProps) {
   return (
-    <div className="flex flex-col grow w-[17rem] max-w-[22rem] h-[26rem] bg-white text-sm font-regular border p-4 mr-6 mb-6 border-ssTheme-orange minDesktop:border-ssTheme-darkerBlue">
+    <div className="flex flex-col grow w-[17rem] max-w-[22rem] h-[26rem] bg-white text-sm font-regular border  p-4 mr-6 mb-6 border-ssTheme-orange minDesktop:border-black/10 ">
       <div className=" flex-grow-0 font-semibold text-lg">{header}</div>
       <div className="flex-grow flex items-center justify-center bg-ssTheme-blue my-3">
         image box with flexible height
